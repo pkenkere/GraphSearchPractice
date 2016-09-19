1dca94685107a1c0b7ffe6427d6c9127843b8224
Matthew Page <matthewpagecs@gmail.com>
2016-09-19 07:32:02 -0400

    Implemented dfs

--- src/graph/Dfs.java
+++ src/graph/Dfs.java
@@ -1,11 +1,28 @@
 package graph;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Dfs {
     public static List<Tile> calculateDFS(Maze maze) {
-        // TODO implement depth first search
-        // Return a path from the start to the end
-        return null;
+        return dfs(maze.getStartTile(), maze, new ArrayList<Tile>());
+    }
+
+    private static List<Tile> dfs(Tile tile, Maze maze, List<Tile> path) {
+        tile.visit();
+        path.add(tile);
+        if (tile.isGoal(maze.getGoalTile().getX(), maze.getGoalTile().getY())) {
+            return path;
+        } else {
+            for (Tile adjTile : maze.getAdjacentTiles(tile)) {
+                if (!adjTile.isVisited()) {
+                    List<Tile> potentialSolution = dfs(adjTile, maze, path);
+                    if (potentialSolution != null)
+                        return potentialSolution;
+                }
+            }
+            path.remove(path.size() - 1);
+            return null;
+        }
     }
 }
