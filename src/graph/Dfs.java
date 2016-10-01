e65bb709f1979bd900b29a08b21d9937bfc44336
Parth Kenkere <pkenkere@sslab06.cs.purdue.edu>
2016-10-01 16:13:07 -0400

    Implemented DFS

--- src/graph/Dfs.java
+++ src/graph/Dfs.java
@@ -1,11 +1,35 @@
 package graph;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class Dfs {
-    public static List<Tile> calculateDFS(Maze maze) {
-        // TODO implement depth first search
-        // Return a path from the start to the end
-        return null;
-    }
+        public static List<Tile> calculateDFS(Maze maze){
+            Tile start = maze.getStartTile();
+            Tile end = maze.getGoalTile();
+            List<Tile> path = new LinkedList<>();
+            DFS(start, end, path, maze);
+            return path;
+
+        }
+
+
+        public static List<Tile> DFS (Tile currentTile, Tile endTile, List <Tile> path, Maze maze){
+            path.add(currentTile);
+            if (currentTile == endTile) {
+                return path;
+            }
+
+            currentTile.visit();
+            List<Tile> list;
+            for (Tile nextTile : maze.getAdjacentTiles(currentTile)) {
+                if (!nextTile.isVisited() && nextTile.isTraversable()) {
+                    if ((list = DFS(nextTile, endTile, path, maze)) != null)
+                        return list;
+
+                }
+            }
+            path.remove(currentTile);
+            return null;
+        }
 }
